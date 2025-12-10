5c8e2bb028a7fe134346e90dd56571a2fb8665f3
Zhengbao Lai <laizhengbao@icloud.com>
2025-12-10 14:11:21 +0800

    feat: BREAKCHANGE: wrapper: nonblock IO
    for nonblocking IO, we added
    - ringbuffer
    - nonblocking read/write
    - ringbuffer to linear buffer
    
    so we changed the function `send_daemon` to be capable to handle nonblocking
    file descripter.

--- wrapper/wrapper.h
+++ wrapper/wrapper.h
@@ -2,6 +2,10 @@
 #ifndef __BRUTALSHELL_WRAPPER_H__
 #define __BRUTALSHELL_WRAPPER_H__
 
+#ifndef BUFLEN
+#define BUFLEN 256
+#endif
+
 #include <termios.h>
 
 #include <unistd.h>
@@ -18,9 +22,17 @@ struct config {
 	size_t len;
 };
 
+struct ring_buffer {
+	size_t head;
+	size_t tail;
+	size_t count;
+	size_t buflen;
+	char *buf;
+};
+
 int connect_daemon( struct config );
 int get_session ( int );
-int send_daemon( int method, int fd, char *restrict, ssize_t );
+ssize_t send_daemon( int method, void *restrict, void *restrict, ssize_t );
 
 int read_pty( void );
 int set_pty( void ); /* raw pty */
@@ -29,6 +41,12 @@ void reset_pty( void );
 void usage( const char *restrict );
 
 struct config get_configure( int, char **restrict );
+
+int set_nonblocking( int );
+ssize_t nb_read( void *restrict, void *restrict );
+ssize_t nb_write( void *restrict, void *restrict );
+ssize_t b_write( int, void *restrict, size_t );
+void *dump_ringbuf( void *restrict, size_t, const void *restrict );
 
 enum LOGLEVVELS {
 	log_error,
